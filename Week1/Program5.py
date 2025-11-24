1c48207c862acdea4b11f6f0ad797c69d833bb44
Aditya Kumar Sharma <sharmaaditya12.02.2004@gmail.com>
2025-11-24 21:55:13 +0530

    typo

--- Week1/Program5.py
+++ Week1/Program5.py
@@ -6,8 +6,9 @@ else:
     print("The no of bytes in the hardisk is ",n_bytes)
     size_img=float(input("Enter the size of image in MB: "))
     if(size_img<=0):
-        print("Invalid Data Entered"):
+        print("Invalid Data Entered")
     else:
         n_bytes_img=1024**2*size_img
         n_imgs=n_bytes//n_bytes_img
         print("The number of images the hardisk can hold is ",n_imgs)
+
